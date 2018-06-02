921623bfbc030bc58bf0aba43cd1824c124a3383
Nestor Valentinis <nesti07@gmail.com>
2018-06-02 11:12:22 -0300

    Nestor - Configure React Router, Create PostIndex and Create Fetch Action Creator

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+
+export const FETCH_POSTS = 'FETCH_POSTS';
+
+export function fetchPosts() {
+
+  return {
+    type: FETCH_POSTS,
+    payload: {}
+  };
+}
---
+++ src/components/posts_index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class PostIdex extends Component {
+
+  render() {
+    return (
+      <div> Post Index </div>
+    );
+  }
+
+}
+
+export default PostIdex;
