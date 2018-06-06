6e7258967cc96a34480a416d437b2d8fe2c5376b
Nestor Valentinis <nesti07@gmail.com>
2018-06-06 20:18:46 -0300

    Nestor - Add redux-form lib, wired it up and  Add the Field componet on the Form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
 
   render() {
     return (
-      <div> Posts New </div>
+      <form>
+        <Field
+            label="Title"
+            name="title"
+            component={ this.renderField }
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={ this.renderField }
+        />
+
+        <Field
+          label="Post Content"
+          name="content"
+          component={ this.renderField }
+        />
+
+      </form>
+    );
+  }
+
+  renderField(field) {
+    return (
+      <div className="form-group" >
+        <label>{ field.label }</label>
+        <input className="form-control"
+          type="text"
+          { ...field.input }
+        />
+      </div>
     );
   }
 
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostNewForm'
+})(PostsNew);
